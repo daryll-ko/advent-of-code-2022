81aeb4d1ec7ce418dee49f4ceaf97db6a45a2f9c
Daryll Ko <daryll.ko@gmail.com>
2022-12-03 09:19:41 +0800

    feat: add 1a

---
+++ 1a.py
@@ -0,0 +1,10 @@
+with open("input.txt", "r") as input_file:
+    lines = input_file.readlines()
+    current, best = 0, 0
+    for line in lines:
+        if line == '\n':
+            best = max(best, current)
+            current = 0
+        else:
+            current += int(line)
+    print(best)
