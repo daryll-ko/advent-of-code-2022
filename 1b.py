5d594a4fa48fa5722e437d8df9c21c47dc673848
Daryll Ko <daryll.ko@gmail.com>
2022-12-03 09:27:17 +0800

    feat: add 1b

---
+++ 1b.py
@@ -0,0 +1,12 @@
+with open("input.txt", "r") as input_file:
+    lines = input_file.readlines()
+    current = 0
+    amounts = []
+    for line in lines:
+        if line == '\n':
+            amounts.append(current)
+            current = 0
+        else:
+            current += int(line)
+    amounts.sort(reverse=True)
+    print(sum(amounts[:3]))
