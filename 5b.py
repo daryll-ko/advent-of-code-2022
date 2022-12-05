c564a0970552fba6abe206f94a964343251a0c73
Daryll Ko <daryll.ko@gmail.com>
2022-12-05 14:28:07 +0800

    feat: finish Day 5 Part 2

---
+++ 5b.py
@@ -0,0 +1,27 @@
+crates = [
+    ['Z', 'T', 'F', 'R', 'W', 'J', 'G'],
+    ['G', 'W', 'M'],
+    ['J', 'N', 'H', 'G'],
+    ['J', 'R', 'C', 'N', 'W'],
+    ['W', 'F', 'S', 'B', 'G', 'Q', 'V', 'M'],
+    ['S', 'R', 'T', 'D', 'V', 'W', 'C'],
+    ['H', 'B', 'N', 'C', 'D', 'Z', 'G', 'V'],
+    ['S', 'J', 'N', 'M', 'G', 'C'],
+    ['G', 'P', 'N', 'W', 'C', 'J', 'D', 'L']
+]
+
+with open("input.txt", "r") as input_file:
+    lines = input_file.readlines()
+    for line in lines:
+        if len(line) >= 4 and line[:4] == "move":
+            _, a, _, b, _, c = line.split()
+            a, b, c = int(a), int(b), int(c)
+            b -= 1
+            c -= 1
+            for _ in range(a):
+                crate = crates[b].pop()
+                crates[c].append(crate)
+            crates[c][-a:] = reversed(crates[c][-a:])
+    for stack in crates:
+        print(stack[-1], end='')
+    print()
