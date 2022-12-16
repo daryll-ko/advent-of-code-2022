68fa3f26579eda521829738167adcd51d1d6fc25
Daryll Ko <daryll.ko@gmail.com>
2022-12-16 13:08:16 +0800

    feat: add setup for Day 16

---
+++ 16_setup.py
@@ -0,0 +1,14 @@
+with open("input.txt", 'r') as input_file:
+    lines = input_file.readlines()
+    print(len(lines))
+    for line in lines:
+        tokens = line.strip().split()
+        u = tokens[1]
+        flow_rate = tokens[4][5:-1]
+        vs = tokens[9:]
+        n = len(vs)
+        for i in range(n):
+            if vs[i][-1] == ',':
+                vs[i] = vs[i][:-1]
+        print(u, flow_rate)
+        print(n, ' '.join(vs))
