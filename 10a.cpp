56671e054d497413e5163e21c6854f0dd32f612b
Daryll Ko <daryll.ko@gmail.com>
2022-12-17 22:20:31 +0800

    refactor: use `advance_cycle` for Day 10 Part 1 as well

--- 10a.cpp
+++ 10a.cpp
@@ -8,25 +8,22 @@ set<int> relevant = {
 
 int main() {
 	int current_cycle = 0, value = 1, answer = 0;
+	auto advance_cycle = [&]() {
+		current_cycle += 1;
+		if (relevant.count(current_cycle)) {
+			answer += current_cycle * value;
+		}
+	};
 	for (int _ = 1; _ <= 143; ++_) {
 		string op;
 		cin >> op;
 		if (op == "noop") {
-			current_cycle += 1;
-			if (relevant.count(current_cycle)) {
-				answer += current_cycle * value;
-			}
+			advance_cycle();
 		} else {
 			int add;
 			cin >> add;
-			current_cycle += 1;
-			if (relevant.count(current_cycle)) {
-				answer += current_cycle * value;
-			}
-			current_cycle += 1;
-			if (relevant.count(current_cycle)) {
-				answer += current_cycle * value;
-			}
+			advance_cycle();
+			advance_cycle();
 			value += add;
 		}
 	}
