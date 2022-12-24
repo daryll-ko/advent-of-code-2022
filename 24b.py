2bbf32f681b04db04a1414d469da98a859f5ec97
Daryll Ko <daryll.ko@gmail.com>
2022-12-24 15:53:12 +0800

    refactor: turn 24a into function

--- 24b.py
+++ 24b.py
@@ -33,72 +33,31 @@ for t in range(1, TIME_LIMIT):
         blizzards[t].append((i_to, j_to, d))
         blizzards_no_d[t].add((i_to, j_to))
 
-t1, t2, t3 = -1, -1, -1
 
-reachable = [[False for _ in range(C)] for _ in range(R)]
-reachable[0][1] = True
-
-for t in range(1, TIME_LIMIT):
-    new_reachable = [[False for _ in range(C)] for _ in range(R)]
-    for i in range(R):
-        for j in range(C):
-            if not within(i, j) or not reachable[i][j]:
-                continue
-            for d in range(4):
-                i_to, j_to = i + DI[d], j + DJ[d]
-                if not within(i_to, j_to):
+def fastest(si: int, sj: int, ti: int, tj: int, time_offset: int) -> int:
+    reachable = [[False for _ in range(C)] for _ in range(R)]
+    reachable[si][sj] = True
+    for t in range(1, TIME_LIMIT):
+        new_reachable = [[False for _ in range(C)] for _ in range(R)]
+        for i in range(R):
+            for j in range(C):
+                if not within(i, j) or not reachable[i][j]:
                     continue
-                if (i_to, j_to) not in blizzards_no_d[t]:
-                    new_reachable[i_to][j_to] = True
-            if (i, j) not in blizzards_no_d[t]:
-                new_reachable[i][j] = True
-    reachable = new_reachable
-    if reachable[R - 1][C - 2]:
-        t1 = t
-        break
+                for d in range(4):
+                    i_to, j_to = i + DI[d], j + DJ[d]
+                    if not within(i_to, j_to):
+                        continue
+                    if (i_to, j_to) not in blizzards_no_d[t + time_offset]:
+                        new_reachable[i_to][j_to] = True
+                if (i, j) not in blizzards_no_d[t + time_offset]:
+                    new_reachable[i][j] = True
+        reachable = new_reachable
+        if reachable[ti][tj]:
+            return t
 
-reachable = [[False for _ in range(C)] for _ in range(R)]
-reachable[R - 1][C - 2] = True
 
-for t in range(1, TIME_LIMIT):
-    new_reachable = [[False for _ in range(C)] for _ in range(R)]
-    for i in range(R):
-        for j in range(C):
-            if not within(i, j) or not reachable[i][j]:
-                continue
-            for d in range(4):
-                i_to, j_to = i + DI[d], j + DJ[d]
-                if not within(i_to, j_to):
-                    continue
-                if (i_to, j_to) not in blizzards_no_d[t1 + t]:
-                    new_reachable[i_to][j_to] = True
-            if (i, j) not in blizzards_no_d[t1 + t]:
-                new_reachable[i][j] = True
-    reachable = new_reachable
-    if reachable[0][1]:
-        t2 = t
-        break
-
-reachable = [[False for _ in range(C)] for _ in range(R)]
-reachable[0][1] = True
-
-for t in range(1, TIME_LIMIT):
-    new_reachable = [[False for _ in range(C)] for _ in range(R)]
-    for i in range(R):
-        for j in range(C):
-            if not within(i, j) or not reachable[i][j]:
-                continue
-            for d in range(4):
-                i_to, j_to = i + DI[d], j + DJ[d]
-                if not within(i_to, j_to):
-                    continue
-                if (i_to, j_to) not in blizzards_no_d[t1 + t2 + t]:
-                    new_reachable[i_to][j_to] = True
-            if (i, j) not in blizzards_no_d[t1 + t2 + t]:
-                new_reachable[i][j] = True
-    reachable = new_reachable
-    if reachable[R - 1][C - 2]:
-        t3 = t
-        break
+t1 = fastest(0, 1, R - 1, C - 2, 0)
+t2 = fastest(R - 1, C - 2, 0, 1, t1)
+t3 = fastest(0, 1, R - 1, C - 2, t1 + t2)
 
 print(t1 + t2 + t3)
